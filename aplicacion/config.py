ef67985abf15a56b4114971c1841e1bd3873f8db
adriangeaa <adriangeavalero@gmail.com>
2025-01-10 08:48:18 +0100

    commit

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True # En despliegue esto pasa a FALSE
-SQLALCHEMY_DATABASE_URI = 'postgresql://tienda_8_2_iok1_user:tlVhpekwqImnjrpZWXcQ07iuQlgujqqx@dpg-ctvp2mbtq21c73ah810g-a.frankfurt-postgres.render.com/tienda_8_2_iok1'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tienda_8_2_iok1_user:tlVhpekwqImnjrpZWXcQ07iuQlgujqqx@dpg-ctvp2mbtq21c73ah810g-a.frankfurt-postgres.render.com/tienda_8_2_iok1'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Ejemplo basico de postgresql
